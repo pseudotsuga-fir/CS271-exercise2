3655aa61d91d24d8a2c5c6d23660d2dc8c983937
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-10-08 18:21:03 -0700

    refactor functions for readability

--- main.rb
+++ main.rb
@@ -1,35 +1,12 @@
-# Name Mangler
+@full_name = "Johanna Jackson"
 
-@name = "Johanna Jackson"
-
-# Mangle the name by reversing it or borgifying it
-def modified_name(choose)
-  # Split the name into first and last in an array
-  split_name = @name.split
-  #reversed_twice = split_name.reverse.reverse
-  #mangled_name = reversed_twice << "Mangled"
-  #return mangled_name * 2
-  # Reverse the array of name components so the array is [last, first]
-  reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
-  new_name1 = reversed_name_components.join(' ')
-  # Store the mangled name
-  @name = new_name1
-  puts new_name1
-  if choose
-    # Split the name into last and first in an array
-    split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
-    reversed_name_components2 = split_name2.reverse
-    # Add borg to the end of the array
-    reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
-    new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
-    @name = new_name2
-  end
-  return @name
+def swap_first_and_last_name(name)
+  name.split.reverse.join(' ')
 end
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+def append_borg_to_string(input_string)
+  input_string << " Borg"
+end
+
+puts "New name: #{swap_first_and_last_name(@full_name)}"
+puts "New borg name: #{append_borg_to_string(swap_first_and_last_name(@full_name))}"
